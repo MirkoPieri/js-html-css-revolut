be1709a963338f6276eb320b3d129297e21b426e
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-18 15:29:41 +0200

    Creazione file principali.

---
+++ script.js
@@ -0,0 +1,3 @@
+// Ricreare almeno l’header completo con i dropdown che al click si mostrano e spariscono ricliccando.
+// La traccia da riprodurre è questa: https://www.revolut.com/en-IT/
+// Poi tutto quello che riuscite a ricreare della pagina è tutto di guadagnato per ripasso e ex ulteriore vostro. Quindi più fate più vi conviene.
